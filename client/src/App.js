8ebed335ba35d2b21d9ddb9bca31c3c10216bbfe
Cody <cody@posteo.de>
2017-09-07 23:31:18 -0400

    add some examples of routing for teaching

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router,
+         Route,
+         Link } from 'react-router-dom'
 import axios from 'axios'
 import './App.css';
 
+function Routes () {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/test1">Test Page 1</Link></li>
+          <li><Link to="/test2">Test Page 2</Link></li>
+          <li><Link to="/test3">Test Page 3</Link></li>
+        </ul>
+
+        { /* THIS IS HOW REACT DOES COMMENTS
+           *
+           * Route components (below) are basically
+           * "replaced" by mounting the component specified in the
+           * component prop when you visit the route specified in the
+           * path prop */ }
+
+        <Route exact path="/test1" component={Test1} />
+        <Route exact path="/test2" component={Test2} />
+        <Route exact path="/test3" component={Test3} />
+      </div>
+    </Router>
+  )
+}
+
+function Test1 () {
+  return (<div>We are on test page #1</div>)
+}
+
+function Test2 () {
+  return (<div>We are on test page #2</div>)
+}
+
+function Test3 () {
+  return (<div>We are on test page #3</div>)
+}
+
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      numbers: []
-    }
-  }
-
-  componentDidMount () {
-    axios.get('/api/test')
-      .then(res => {
-        this.setState({numbers: res.data.numbers})
-      })
-  }
-
   render() {
-    const numbers = this.state.numbers
-    const showNumbers = numbers.map(n => (<div key={n.toString()}>{ n }</div>))
-
     return (
-      <div>
-        {showNumbers}
-        <p> If this shows "one two three four" everything is setup properly</p>
-      </div>
-    );
+      <Routes />
+    )
   }
 }
 
