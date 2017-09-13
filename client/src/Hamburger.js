a8f788fddf51c3a10b5d0889c07b8da32bd241f4
Cody <cody@posteo.de>
2017-09-12 20:46:07 -0400

    remove unneeded click event on hamburer menu

--- client/src/Hamburger.js
+++ client/src/Hamburger.js
@@ -6,24 +6,12 @@ import hamburger from './styles/hamburger.css'
 
 class Hamburger extends Component {
 
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      toggleMenu: false
-    }
-  }
-
-  toggleMenu (e) {
-    this.setState({toggleMenu: false})
-  }
-
   render () {
     return (
-      <Menu isOpen={ this.state.toggleMenu } >
-        <Link id="home" onClick={ this.toggleMenu.bind(this) } className="bm-link" to="/">Home</Link>
-        <Link id="login" onClick={ this.toggleMenu.bind(this) } className="bm-link" to="/login">Login</Link>
-        <Link id="about" onClick={ this.toggleMenu.bind(this) } className="bm-link" to="/about">About</Link>
+      <Menu isOpen={ false } >
+        <Link id="home" className="bm-link" to="/">Home</Link>
+        <Link id="login" className="bm-link" to="/login">Login</Link>
+        <Link id="about" className="bm-link" to="/about">About</Link>
       </Menu>
     )
   }
