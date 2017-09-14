6ae4fd21bfd7ec9faa9fe910046a085d438c02f5
Cody <cody@posteo.de>
2017-09-13 20:08:57 -0400

    highlight hamburger item when we are on it's route

--- client/src/Hamburger.js
+++ client/src/Hamburger.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { elastic as Menu } from 'react-burger-menu'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 // eslint-disable-next-line
 import hamburger from './styles/hamburger.css'
 
@@ -9,9 +9,9 @@ class Hamburger extends Component {
   render () {
     return (
       <Menu isOpen={ false } >
-        <Link id="home" className="bm-link" to="/">Home</Link>
-        <Link id="login" className="bm-link" to="/login">Login</Link>
-        <Link id="about" className="bm-link" to="/about">About</Link>
+        <NavLink exact id="home" className="bm-link" activeClassName="bm-link-highlight" to="/">Home</NavLink>
+        <NavLink id="login" className="bm-link" activeClassName="bm-link-highlight" to="/login">Login</NavLink>
+        <NavLink id="about" className="bm-link" activeClassName="bm-link-highlight" to="/about">About</NavLink>
       </Menu>
     )
   }
