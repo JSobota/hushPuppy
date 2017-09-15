4a5f5a32dff5057ecfbc124f3aa29cd398a45d81
Cody <cody@posteo.de>
2017-09-14 21:05:17 -0400

    change routes for the new login/signup form

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router,
          Route } from 'react-router-dom'
 import Navbar from './Navbar'
 import LoginForm from './Loginform'
+import SignupForm from './SignupForm'
+import Home from './Home'
 function Routes () {
   return (
     <Router>
       <div>
-        <Navbar />
-        <Route path="/login" component={LoginForm} />
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/signup" component={SignupForm} />
       </div>
     </Router>
   )
