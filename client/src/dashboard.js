da4a91cac416d73b75775b1db39cdc8de55f19d6
Justin Sobota <jusitn.sobota0227@gmail.com>
2017-09-16 11:56:38 -0400

    dashboard page initial start

---
+++ client/src/dashboard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import React, { Component } from 'react';
+import './styles/dashboard.css'
+
+class dashboardSearch extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      id:"",
+    }
+    sendId (e) {
+    e.preventDefault()
+    const payload = {...this.state}
+    /*
+      ajax.post(/api/login-thing, payload)
+        .then(...)
+        .catch(...)
+    */
+  }
+
+  
+render () {
+	return (
+		<div id="dashboard">
+		<div className="groupBox">
+		</div>
+		<h2>Find a Group</h2>
+		<input value={this.state}type="text" name="search" placeholder="Search..">
+		<input type="submit" className="button" onClick={this.sendId.bind(this)} action="submit" value ="Search"/>
+		<h2>or</h2>
+		<input type="submit" className="button" onClick={this.create.bind(this)} action="submit" value="Create"/>
+		</div>
+		)
+}
+
+
+
+
