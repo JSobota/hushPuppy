8acaf36b853dba14ef5ffec5b7eb286a6699c2d6
Cody <cody@posteo.de>
2017-09-16 12:21:58 -0400

    fix errors in dashboard component

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -2,38 +2,41 @@ import React from 'react';
 import React, { Component } from 'react';
 import './styles/dashboard.css'
 
-class dashboardSearch extends Component {
+class DashboardSearch extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      id:"",
+      id:""
     }
-    sendId (e) {
+  }
+
+  sendId(e) {
     e.preventDefault()
     const payload = {...this.state}
     /*
       ajax.post(/api/login-thing, payload)
-        .then(...)
-        .catch(...)
+      .then(...)
+      .catch(...)
     */
   }
 
-  
-render () {
-	return (
-		<div id="dashboard">
-		<div className="groupBox">
-		</div>
-		<h2>Find a Group</h2>
-		<input value={this.state}type="text" name="search" placeholder="Search..">
-		<input type="submit" className="button" onClick={this.sendId.bind(this)} action="submit" value ="Search"/>
-		<h2>or</h2>
-		<input type="submit" className="button" onClick={this.create.bind(this)} action="submit" value="Create"/>
-		</div>
-		)
+  render() {
+    return (
+      <div id="dashboard">
+        <GroupDisplay />
+        <h2>Find a Group</h2>
+        <input value={this.state}type="text" name="search" placeholder="Search.." />
+        <input type="submit" className="button" onClick={this.sendId.bind(this)} action="submit" value ="Search"/>
+        <h2>or</h2>
+        <input type="submit" className="button" onClick={this.create.bind(this)} action="submit" value="Create"/>
+      </div>
+    )
+  }
 }
 
-
-
-
+function GroupDisplay() {
+  return (
+    <div className="groupBox"></div>
+  )
+}
