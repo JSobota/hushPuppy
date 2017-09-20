f96615e85485493e18f31049e07d8cfc56f9fc5d
Cody <cody@posteo.de>
2017-09-19 23:25:59 -0400

    add back tests for group display

--- client/src/GroupDisplay.test.js
+++ client/src/GroupDisplay.test.js
@@ -3,6 +3,25 @@ import { shallow, mount } from 'enzyme'
 import GroupDisplay from './GroupDisplay'
 
 describe('Rendering', () => {
-  // TODO: Add tests for this
-  expect("TODO: Add tests for this").toBe("Done")
+  it('should say "No Groups" when given empty group', () => {
+    const groups = []
+    const component = mount(
+      <GroupDisplay groups={groups} />
+    )
+    expect(component.find('.group-box').html()).toContain('No Groups')
+  })
+
+  it('should render the same number of list elements as groups', () => {
+    const groups = [
+      {id: 1, name: "name", endDate: "12/34/56"},
+      {id: 2, name: "name", endDate: "12/34/56"},
+      {id: 3, name: "name", endDate: "12/34/56"},
+      {id: 4, name: "name", endDate: "12/34/56"},
+      {id: 5, name: "name", endDate: "12/34/56"},
+    ]
+    const component = mount(
+      <GroupDisplay groups={groups} />
+    )
+    expect(component.find('.group-box ul li').length).toBe(5)
+  })
 })
