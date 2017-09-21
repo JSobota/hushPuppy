9779d389cc131eccc34e28e25e69348b37d85a38
Cody <cody@posteo.de>
2017-09-20 21:46:14 -0400

    redirect to dashboard on successful login

--- client/src/Loginform.js
+++ client/src/Loginform.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 import './styles/loginform.css'
 
 class LoginForm extends Component {
@@ -8,7 +9,8 @@ class LoginForm extends Component {
     super(props)
     this.state = {
       name: "",
-      password: ""
+      password: "",
+      redirect: false
     }
   }
 
@@ -27,17 +29,23 @@ class LoginForm extends Component {
       password: this.state.password
     }
     axios.post('/api/user', data)
-      .then(r => console.log(r))
+      .then(res => {
+        if (res.data.success) {
+          this.setState({redirect: true})
+        }
+      })
       .catch(err => console.log(err))
   }
 
   render () {
     return (
-      <form id="loginform" className="form">
+      this.state.redirect ? <Redirect to="dashboard" /> :
+        <form id="loginform" className="form">
         <input type="text" className="input" value={this.state.name} onChange={this.updateName.bind(this)} name="username" placeholder="username" />
         <input type="password" className="input" value={this.state.password} onChange={this.updatePassword.bind(this)} name="password" placeholder="password"/>
         <input type="submit" className="button" onClick={this.sendLogin.bind(this)} action="submit" value ="Login"/>
-      </form>
+        </form>
+
     )
   }
 }
