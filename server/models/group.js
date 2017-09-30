eaef431bda06e8a441a5a5848d8954f40ad072fc
Dan Meyer <somesing9@gmail.com>
2017-09-30 12:10:45 -0400

    Added api routes for messages. Switch force to true to recreate database, turn force off, recreate seed the database

--- server/models/group.js
+++ server/models/group.js
@@ -14,6 +14,10 @@ module.exports = function(sequelize, Sequelize) {
     endDate: {
       type: Sequelize.DATE
     },
+    isMatched: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    }
   });
 
   Group.associate = function(models) {
--- server/routes/api/group.js
+++ server/routes/api/group.js
@@ -1,6 +1,8 @@
 const models = require('../../models/');
 const Group = models.Group;
 const User = models.User;
+const Message = models.Message;
+// const { User, Group, Message } = models;
 
 module.exports = function(router) {
   router.route('/group')
@@ -12,20 +14,20 @@ module.exports = function(router) {
         })
         .then(newGroup => {
           // This needs to be switched to req.user.id
-          newGroup.addUser(req.body.id,{ through: { role: 'admin' }});
+          newGroup.addUser(req.body.id, { through: { role: 'admin' } });
           res.status(201).json(newGroup);
         })
         .catch(error => { res.status(400).send(error) });
     })
 
     // Get a list of all the groups, and their end dates
-    .get(function(req,res){
+    .get(function(req, res) {
       Group.findAll({
-        attributes: ['id', 'name', 'endDate', 'createdAt']
-      })
-      .then(results => {
-        res.status(201).send(results);
-      })
+          attributes: ['id', 'name', 'endDate', 'createdAt']
+        })
+        .then(results => {
+          res.status(201).send(results);
+        })
     })
   // router.use(function(req, res, next) {
   //   if (req.isAuthenticated()) {
@@ -49,9 +51,9 @@ module.exports = function(router) {
           }
           // This needs to be switched to req.user.id
           // 
-          result.addUser(req.body.id).then( () => {
+          result.addUser(req.body.id).then(() => {
             res.status(200).send({ success: true, msg: 'You have joined: ' + result.name });
-          }) 
+          })
           // 
           result.addUser(req.body.id);
           res.status(200).send({ success: true, msg: 'You have joined: ' + result.name });
@@ -66,25 +68,64 @@ module.exports = function(router) {
     // Get a list of all memebers, message, and end date
     .get(function(req, res) {
       Group.findById(req.params.id).then(group => {
-        if (group) {
-          group.getUsers({attributes: ['id', 'firstname', 'lastname', 'email'] }).then(members => {
-            // group.people = members;
-            console.log(group);
-            group = JSON.parse(JSON.stringify(group));
+          if (group) {
+            group.getUsers({ attributes: ['id', 'firstname', 'lastname', 'email'] }).then(members => {
+              // group.people = members;
+              console.log(group);
+              group = JSON.parse(JSON.stringify(group));
+              Object.assign(group, { members: members });
+              Message.findAll({
+                where: {
+                  GroupId: req.params.id
+                }
+              }).then(messages => {
+                res.status(200).send((Object.assign(group, { messages: messages })));
+              })
 
-            res.status(200).send( (Object.assign(group, {members: members})) );
-            // res.status(200).send( {...group, members: members} );
-          })
-        }
-        if (!group) {
-          res.status(200).send({success: false, msg: 'No group found'});
-        }
-        
-      })
-      .catch(error => {
-        restatus(400).send(error);
-      })
+              // res.status(200).send((Object.assign(group, { members: members })));
+            })
+          }
+          if (!group) {
+            res.status(200).send({ success: false, msg: 'No group found' });
+          }
+        })
+        .catch(error => {
+          restatus(400).send(error);
+        })
     })
 
+  router.route('/group/:id/message')
+    // Create messages
+    .post(function(req, res) {
+      Message.create({
+          message: req.body.message,
+          UserId: req.body.uid,
+          GroupId: req.params.id
+        })
+        .then(newMessage => {
+          res.status(200).send({ success: true, msg: 'Message created successfully' })
+        })
+        .catch(error => {
+          res.status(400).send(error);
+        })
+    })
 
+    // Get messages
+    .get(function(req, res) {
+      Group.findById(req.params.id).then(group => {
+          if (group) {
+            group.getMessages().then(messages => {
+              console.log(group);
+              console.log(messages);
+              res.status(200).send(messages);
+            })
+          }
+          if (!group) {
+            res.status(200).send({ success: false, msg: 'No messages found' });
+          }
+        })
+        .catch(error => {
+          res.status(400).send(error);
+        })
+    })
 }
