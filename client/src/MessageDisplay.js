bbb21c6472ea9eeb2e7f03205b25fe9fcd5f2358
Cody <cody@posteo.de>
2017-10-03 23:28:25 -0400

    make message display show real messages

--- client/src/MessageDisplay.js
+++ client/src/MessageDisplay.js
@@ -10,16 +10,41 @@ class MessageInput extends Component {
   }
 
   render() {
-    return <div> jjjjj</div>
+    return (
+      <div>
+        <input type="text" />
+        <button>Submit</button>
+      </div>
+    )
   }
 }
 
-class Messages extends Component {}
+function Messages(props) {
+  const messages = props.messages.map(message => <Message {...message} />)
+  return <div> {messages} </div>
+}
 
-function Message(props) {}
+function Message(props) {
+  const name = `${props.firstName} ${props.lastName}`
+  const message = props.message
+  return (
+    <div>
+      <div> {name} </div>
+      <div> {message}</div>
+    </div>
+  )
+}
 
 function MessageDisplay(props) {
-  return <MessageInput />
+  const messages = [
+    { firstName: 'first', lastName: 'last', message: 'messagerino' }
+  ]
+  return (
+    <div>
+      <Messages messages={messages} />
+      <MessageInput />
+    </div>
+  )
 }
 
 export default MessageDisplay
