da7b6b41b7d9898a14477b494e605d9177e4a786
Cody <cody@posteo.de>
2017-10-05 00:43:12 -0400

    add login protections on certain routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './Navbar'
 import LoginForm from './Loginform'
 import SignupForm from './SignupForm'
@@ -8,20 +8,23 @@ import Dashboard from './Dashboard'
 import CreateForm from './CreateForm'
 import Logout from './Logout'
 import GroupPage from './GroupPage'
+import LoginRequired from './LoginRequired'
 
 function Routes() {
   return (
     <Router>
       <div>
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={LoginForm} />
-        <Route exact path="/signup" component={SignupForm} />
-        {/* add auth to dashboard route later*/}
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/new" component={CreateForm} />
-        <Route exact path="/logout" component={Logout} />
-        <Route exact path="/group/:group" component={GroupPage} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={LoginForm} />
+          <Route exact path="/signup" component={SignupForm} />
+          {/* add auth to dashboard route later*/}
+          <LoginRequired exact path="/dashboard" component={Dashboard} />
+          <LoginRequired exact path="/new" component={CreateForm} />
+          <Route exact path="/logout" component={Logout} />
+          <LoginRequired exact path="/group/:group" component={GroupPage} />
+        </Switch>
       </div>
     </Router>
   )
