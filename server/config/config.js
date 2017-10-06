36a3971c7fb709e68bc4f125cd7c04b955aad24d
Cody <cody@posteo.de>
2017-10-05 20:03:35 -0400

    add postgres to test config
    use it by setting the nodeenv to test

--- server/config/config.js
+++ server/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.T_DB_USER,
+    "password": process.env.T_DB_PASS,
+    "database": process.env.T_DB_DATABASE,
+    "host": process.env.T_DB_HOST,
+    "dialect": "postgres"
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
